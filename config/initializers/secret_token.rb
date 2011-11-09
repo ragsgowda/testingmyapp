9e7ec1daca1f2317624e038239bfb460453bfa93
Vavia Technologies <vavia@Vavias-Mac-mini.local>
2011-11-09 17:25:46 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testingmyapp::Application.config.secret_token = 'b4533138cd8d107c0e1d234c7a919d3a9e6a5d9fe616854014e1dbd16afe62eb77ad9f2b6c31ea509a867a0f171c2bfaf75ceaca5b2f3c919c8fbf76735c6c17'
